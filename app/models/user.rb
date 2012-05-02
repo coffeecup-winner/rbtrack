cec9fc47c4c78a7bec3ac60e55ec35b4d0b6a904
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2012-05-02 19:56:23 +0400

    Added User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,12 @@
 #
 
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   attr_accessible :email, :name
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  before_save -> user { user.email.downcase! }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,51 @@ describe User do
   subject { @user }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should be_valid }
+  describe 'when name is not present' do
+    before { @user.name = '' }
+    it { should_not be_valid }
+  end
+  describe 'when email is not present' do
+    before { @user.email = '' }
+    it { should_not be_valid }
+  end
+  describe 'when name is too long' do
+    before { @user.name = 'x' * 31 }
+    it { should_not be_valid }
+  end
+  describe 'invalid email formats' do
+    it 'should be invalid' do
+      addresses = %w[user@example,com user_example_com user_example.com user.name@
+        user.name@com user.name@.com user.name@example.]
+      addresses.each do |address|
+        @user.email = address
+        @user.should_not be_valid
+      end
+    end
+  end
+  describe 'valid email formats' do
+    it 'should be valid' do
+      addresses = %w[user@example.com user.name@example.com user.name@example.co.uk
+        USER+NAME@example.com]
+      addresses.each do |address|
+        @user.email = address
+        @user.should be_valid
+      end
+    end
+  end
+  describe 'when the email address is not unique' do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+    it { should_not be_valid }
+  end
+  it 'should be saved with lowercase email' do
+    @user.email = 'SHOULD.BE.LOWERCASE@EMAIL.ADDRESS'
+    @user.should be_valid
+    @user.save
+    @user.email.should == 'should.be.lowercase@email.address'
+  end
 end
