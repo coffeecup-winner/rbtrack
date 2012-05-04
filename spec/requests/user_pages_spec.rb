1417347178c6ab6db4e518ea9debeb8fe081ea47
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2012-05-05 00:59:58 +0400

    Add tests, improve code

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,6 +11,12 @@ describe 'User pages' do
       it 'should not create a user' do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe 'error messages' do
+        before { click_button submit }
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+        it { should_not have_content('digest') }
+      end
     end
     describe 'with valid information' do
       before do
@@ -21,6 +27,12 @@ describe 'User pages' do
       end
       it 'should create a user' do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      describe 'after saving the user' do
+        before { click_button submit }
+        let(:user) { User.find_by_email('fred@example.com') }
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
