29feed43652e35d64fa97e1f22dd8df118e5ea0a
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2012-05-08 03:49:11 +0400

    Hide sign up link on main page if user is logged in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,5 +10,12 @@ describe "Static pages" do
       click_link 'Sign up'
       page.should have_selector('h1', text: 'Sign up')
     end
+    describe 'sign up button' do
+      it { should have_link('Sign up', href: signup_path) }
+      describe 'when user is logged in' do
+        before { sign_in FactoryGirl.create(:user) }
+        it { should_not have_link('Sign up') }
+      end
+    end
   end
 end
